95430cce04edb7ebb6615b4df6ff874aa7405a8e
CJW-MAPU <c.j.biz.woong@gmail.com>
2021-05-22 19:43:44 +0900

    #add Project

---
+++ server/src/test/java/kr/ac/pcu/cyber/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.pcu.cyber.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
