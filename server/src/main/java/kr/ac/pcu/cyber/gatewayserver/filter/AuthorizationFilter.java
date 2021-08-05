f9ba65a4d26040cba4a7eddce5ccec10b6e9a49c
dhslrl321 <dhslrl321@gmail.com>
2021-08-05 18:32:38 +0900

    #update application.yml StudentService Predicates 와 Filter 추가

--- server/src/main/java/kr/ac/pcu/cyber/gatewayserver/filter/AuthorizationFilter.java
+++ server/src/main/java/kr/ac/pcu/cyber/gatewayserver/filter/AuthorizationFilter.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-
 import java.util.Locale;
 
 @Component
